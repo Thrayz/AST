3775b927ee56280e30faf9a0e646afe7d6751fca
Thrayz <traidihamza0@gmail.com>
2024-03-29 08:34:43 +0100

    minor changes

---
+++ AST.Server/Controllers/GoalsController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using AST.Server.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace AST.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GoalsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GoalsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Goal>>> GetGoals()
+        {
+            return await _context.Goals.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Goal>> GetGoal(int id)
+        {
+            var goal = await _context.Goals.FindAsync(id);
+
+            if (goal == null)
+            {
+                return NotFound();
+            }
+
+            return goal;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutGoal(int id, Goal goal)
+        {
+            if (id != goal.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(goal).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!GoalExists(id))
+                {
+                    return NotFound();
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Goal>> PostGoal(Goal goal)
+        {
+            _context.Goals.Add(goal);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetGoal", new { id = goal.Id }, goal);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteGoal(int id)
+        {
+            var goal = await _context.Goals.FindAsync(id);
+            if (goal == null)
+            {
+                return NotFound();
+            }
+
+            _context.Goals.Remove(goal);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool GoalExists(int id)
+        {
+            return _context.Goals.Any(e => e.Id == id);
+        }   
+
+    }
+}
---
+++ AST.Server/Controllers/SocialInteractionsController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using AST.Server.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AST.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SocialInteractionsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SocialInteractionsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<SocialInteraction>>> GetSocialInteractions()
+        {
+            return await _context.SocialInteractions.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<SocialInteraction>> GetSocialInteraction(int id)
+        {
+            var socialInteraction = await _context.SocialInteractions.FindAsync(id);
+
+            if (socialInteraction == null)
+            {
+                return NotFound();
+            }
+
+            return socialInteraction;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutSocialInteraction(int id, SocialInteraction socialInteraction)
+        {
+            if (id != socialInteraction.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(socialInteraction).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SocialInteractionExists(id))
+                {
+                    return NotFound();
+                }
+            }
+
+            return NoContent();
+        }
+
+        private bool SocialInteractionExists(int id)
+        {
+            return _context.SocialInteractions.Any(e => e.Id == id);
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult<SocialInteraction>> PostSocialInteraction(SocialInteraction socialInteraction)
+        {
+            _context.SocialInteractions.Add(socialInteraction);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetSocialInteraction", new { id = socialInteraction.Id }, socialInteraction);
+        }
+
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteSocialInteraction(int id)
+        {
+            var socialInteraction = await _context.SocialInteractions.FindAsync(id);
+            if (socialInteraction == null)
+            {
+                return NotFound();
+            }
+
+            _context.SocialInteractions.Remove(socialInteraction);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        
+
+    }
+}
